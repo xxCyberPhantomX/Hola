5b8fb460b6c8e0355ad0910ab08e54e739c7aed4
epunalesg@miumg.edu.gt <167832695+xxCyberPhantomX@users.noreply.github.com>
2024-10-04 23:02:31 -0600

    holamundo

---
+++ Hola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyect.hola;
+
+/**
+ *
+ * @author Samuel Puñales
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
